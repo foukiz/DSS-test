corrected major bugs (related to normalization layers)

datasets/seq_cifar10.py
@@ -71,7 +71,8 @@ class sCIFAR10(Dataset):
         return self.val_ds
 
     def import_dataset(self):
-        torch.manual_seed(42)
+        #torch.manual_seed(42)
+        gen = torch.Generator().manual_seed(42)
 
         # this transform allows to download the cifar10 images in the flattened shape
         transform = transforms.Compose([
@@ -85,7 +86,8 @@ class sCIFAR10(Dataset):
 
         train_ds, val_ds = torch.utils.data.random_split(
             datasets.CIFAR10("data/cifar10_data", train=True, download=True, transform=transform),
-            [self.train_size, self.val_size]
+            [self.train_size, self.val_size],
+            generator=gen
         )
         test_ds = datasets.CIFAR10("data/cifar10_data", train=False, download=True, transform=transform)
 

kernels.py
@@ -37,8 +37,8 @@ class DSSKernel(nn.Module):
         self.sep_dt_re_im = sep_dt_re_im
         self.Lambda_init = Lambda_init
 
-        # complex tensors are stored as real with an extra last dim of size 2 
-        # to denote real, imag parts as ADAM moments are non-linear  
+        # complex tensors are stored as real with an extra last dim of size 2
+        # to denote real, imag parts as ADAM moments are non-linear
         log_dt, Lambda, W = self.init(N, H, dt_min, dt_max, Lambda_init)  # [H], [N 2], [H N 2]
 
         self.register_parameter('log_dt', torch.nn.Parameter(log_dt))
@@ -101,9 +101,7 @@ class DSSKernel(nn.Module):
             dt_Lambda = self.log_dt.exp().unsqueeze(-1) * Lambda             # [H N]
 
         P = dt_Lambda.unsqueeze(-1) * torch.arange(L, device=W.device)       # [H N L]
-        # replace the sequence length in first dimension
-        P = P.permute(-1, *range(P.ndim - 1))                               # [L H N]
-
+        
         if self.version in ['softmax']:
             # fast softmax using structure of P
             # see Appendix A.2 in https://arxiv.org/abs/2203.14343
@@ -111,8 +109,8 @@ class DSSKernel(nn.Module):
             if Lambda_gt_0.any():
                 with torch.no_grad():
                     P_max = dt_Lambda * (Lambda_gt_0 * (L-1))                # [H N]
-                P = P - P_max.unsqueeze(0)                                  # [L H N]
-            S = P.exp()                                                      # [L H N]
+                P = P - P_max.unsqueeze(-1)                                  # [H N L]
+            S = P.exp()                                                      # [H N L]
 
             dt_Lambda_neg = dt_Lambda * (1 - 2*Lambda_gt_0)                  # [H N]
             # 1 / S.sum(-1) == num / den
@@ -120,13 +118,11 @@ class DSSKernel(nn.Module):
             den = (dt_Lambda_neg * L).exp() - 1                              # [H N]
             W = W * num * reciprocal(den * Lambda, self.epsilon)             # [H N]
         else:
-            S = P.exp()                                                      # [L H N]
+            S = P.exp()                                                      # [H N L]
             if 'no-scale' not in self.version:
                 W = W * (dt_Lambda.exp() - 1.) * reciprocal(Lambda, clamp=True)  # [H N]
 
-        # version papier DSS, qui retourne l'état caché (mais ne sert à rien)
-        #return oe.contract('hn,lhn->lh', W, S).real.to(torch.float32), state     # [L H]
-        return oe.contract('hn,lhn->lh', W, S).real.to(torch.float32)            # [L H]
+        return oe.contract('hn,hnl->hl', W, S).float()            # [H L]
 
 
 
@@ -186,7 +182,7 @@ class GammaExpectationKernel(nn.Module):
         beta = 1. / theta + Delta * torch.arange(L+1, device=theta.device).unsqueeze(-1) # [L+1 H]
         k = d * theta**(-alpha-1.) / alpha * (beta[:-1,...]**(-alpha) - beta[1:,...]**(-alpha))                # [L H]
 
-        return k
+        return k.transpose(0,1)  # [H L]
     
     def Order2DL_forward(self, L, state=None):
         raise NotImplementedError
@@ -233,7 +229,7 @@ class ExponentialExpectationKernel(nn.Module):
         j = torch.arange(L, device=alpha.device).unsqueeze(1)
         k = d * alpha * torch.log(1 + Delta / (j * Delta + alpha))
 
-        return k
+        return k.transpose(0,1)  # [H L]
 
     def Order2DL_forward(self, L, state=None):
         raise NotImplementedError

launch.py
@@ -4,6 +4,9 @@ import os
 os.environ["WANDB_MODE"] = "online" if has_internet() else "offline"
 
 import torch
+import random
+import numpy as np
+import os
 
 import argparse
 import wandb
@@ -12,9 +15,11 @@ import time
 from datetime import datetime, timedelta
 import yaml
 
+from typing import Optional
+
 from training import train, evaluate
 from config import Config
-from models import DSS, S4, Transformer
+from models import DSS, S4, TransformerEncoder
 
 from datasets import copy_task, listops, seq_cifar10, imdb, aan, pathfinder, smnist, pmnist, ptb
 
@@ -29,6 +34,7 @@ def parse_args():
     parser.add_argument("--use_wandb", action="store_true")
     parser.add_argument("--use_tqdm", action="store_true")
     parser.add_argument("--no_train", action="store_true")
+    parser.add_argument("--pre_seed", action="store_true")
     parser.add_argument("--device", type=str, default='cpu')
     parser.add_argument("--save_network", action="store_true")
     parser.add_argument("--save_name", type=str, default=None)
@@ -40,7 +46,7 @@ ARGS = vars(parse_args())
 
 def make_model(name, **kwargs):
     low_name = name.lower()
-    models = {'dss': DSS, 's4': S4, 'transformer': Transformer}
+    models = {'dss': DSS, 's4': S4, 'transformer': TransformerEncoder}
     if low_name not in models:
         err_str = "{} is not a correct model name, accepted models are".format(low_name)
         for i, k in enumerate(models.keys()):
@@ -106,6 +112,7 @@ def launch(
     config=None,
     use_wandb=False,
     use_tqdm=True,
+    pre_seed=False,
     device='cpu',
     save_network=False,
     save_name=None,
@@ -147,6 +154,10 @@ def launch(
         else:
             print("Warning: cuda device specified but not available, using cpu instead")
             device = 'cpu'
+
+    if pre_seed:
+        seed_everything(cfg.model['seed'], workers=True)
+        cfg.model.pop('seed', None)
 
     kwargs = {}
     dataset = make_dataset(**cfg.dataset)
@@ -222,6 +233,47 @@ def multiple_launch(config=None, **kwargs):
     for cfg in conf_iterator:
         launch(config=cfg, **kwargs)
 
+
+
+def seed_everything(seed: Optional[int] = None, workers: bool = False) -> int:
+    r"""Function that sets the seed for pseudo-random number generators in: torch, numpy, and Python's random module.
+    In addition, sets the following environment variables:
+
+    - ``PL_GLOBAL_SEED``: will be passed to spawned subprocesses (e.g. ddp_spawn backend).
+    - ``PL_SEED_WORKERS``: (optional) is set to 1 if ``workers=True``.
+
+    Args:
+        seed: the integer value seed for global random state in Lightning.
+            If ``None``, it will read the seed from ``PL_GLOBAL_SEED`` env variable. If ``None`` and the
+            ``PL_GLOBAL_SEED`` env variable is not set, then the seed defaults to 0. If seed is
+            not in bounds or cannot be cast to int, a ValueError is raised.
+        workers: if set to ``True``, will properly configure all dataloaders passed to the
+            Trainer with a ``worker_init_fn``. If the user already provides such a function
+            for their dataloaders, setting this argument will have no influence. See also:
+            :func:`~lightning_fabric.utilities.seed.pl_worker_init_function`.
+        verbose: Whether to print a message on each rank with the seed being set.
+
+    """
+    if seed is None:
+        env_seed = os.environ.get("PL_GLOBAL_SEED")
+        if env_seed is None:
+            seed = 0
+        else:
+            try:
+                seed = int(env_seed)
+            except ValueError:
+                raise ValueError(f"Invalid seed specified via PL_GLOBAL_SEED: {repr(env_seed)}")
+    elif not isinstance(seed, int):
+        seed = int(seed)
+
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    os.environ["PL_SEED_WORKERS"] = f"{int(workers)}"
+
+    return seed
 
 
 

layers.py
@@ -18,6 +18,8 @@ class DSSLayer(nn.Module):
         input_size,
         state_size,
         bias=True,
+        activation=None,
+        dropout=0.0,
         version='exp',
         bidirectional=False,
         seed=None,
@@ -25,7 +27,7 @@ class DSSLayer(nn.Module):
         **kwargs
     ):  
         assert version in self.VERSIONS, "version must be one of {}".format(self.VERSIONS)
-        if seed: torch.manual_seed(seed)
+        #if seed: torch.manual_seed(seed)
         super().__init__()
 
         self.h = input_size
@@ -44,7 +46,13 @@ class DSSLayer(nn.Module):
             self.kernel = UniformExpectationKernel(self.h, **kwargs)
         elif version == 'exponential':
             self.kernel = ExponentialExpectationKernel(self.h, **kwargs)
-        self.bias = bias
+        #self.bias = bias
+
+        # should have been instantiated already
+        self.activation = activation
+        self.dropout = nn.Dropout2d(dropout) if dropout > 0.0 else nn.Identity()
+
+        self.linear = nn.Linear(input_size, input_size, bias=bias)
 
     def forward(self, u): # absorbs return_output and transformer src mask
         """
@@ -55,26 +63,28 @@ class DSSLayer(nn.Module):
         """
 
         # L (sequence length) is the second dimension, the first is the batch size
-        L = u.size(1)
+        L = u.size(-1)
 
         # Compute SS Kernel
         Lk = L if not self.max_kernel_length else min(self.max_kernel_length, L)
-        k = self.kernel(L=Lk)  # (Lk H)
+        k = self.kernel(L=Lk)  # (H Lk)
 
         # y = multiply_polynomials(u.unsqueeze(1), k.unsqueeze(0))[..., :L]  # (B 1 H L), (1 H Lk) -> (B H L)
-        # fft has to be performed along the seuquence length dimension ;
-        # hence the arguments dim=0 and dim=1 respectively in k_f and u_f
         n = L + Lk
-        k_f = torch.fft.rfft(k, dim=0, n=n)  # (~n/2 H)
-        u_f = torch.fft.rfft(u, dim=1, n=n)  # (B ~n/2 H)
-        y_f = k_f[None,:,:] * u_f            # (B ~n/2 H)
-        y = torch.fft.irfft(y_f, dim=1, n=n)[:,:L,:] # (B L H)
+        k_f = torch.fft.rfft(k, n=n)  # (H ~n/2)
+        u_f = torch.fft.rfft(u, n=n)  # (B H ~n/2)
+        y_f = oe.contract('bhl,hl->bhl', u_f, k_f)            # (B H ~n/2)
+        y = torch.fft.irfft(y_f, n=n)[... ,:L] # (B H L)
 
         # Compute D term in state space equation - essentially a skip connection
-        y = y + oe.contract('blh,h->blh', u, self.D)
-        #y = y + u * self.D[None,None,:]  # (B L H)
+        y = y + oe.contract('bhl,h->bhl', u, self.D)
+        #y = y + u * self.D[None,None,:]  # (B H L)
 
-        return y
+        y = self.dropout(self.activation(y))  # (B H L)
+
+        y = self.linear(y.transpose(-1, -2)).transpose(-1, -2)  # (B H L)
+
+        return y        # (B H L)
 
     @property
     def d_state(self):
@@ -150,7 +160,6 @@ class InputEncoder(nn.Module):
 
 
 
-
 class TopPooling(nn.Module):
     """ A layer to put on top of the sequence model that outputs a sequence to extract
         a single vector out of the sequence, or the whole sequence.
@@ -182,26 +191,63 @@ class Normalization(nn.Module):
         super().__init__()
         self.input_size = input_size
         self.mode = mode if mode is not None else 'none'
-        self.transpose = False
 
         assert mode in ['batch_norm', 'layer_norm', 'none'], "mode must be one of ['batch_norm', 'layer_norm', 'none']"
 
         if mode == 'batch_norm':
+            #self.norm = TransposeBatchNorm(input_size)
             self.norm = nn.BatchNorm1d(input_size)
-            self.transpose = True
         elif mode == 'layer_norm':
-            self.norm = nn.LayerNorm(input_size)
+            self.norm = CustomLayerNorm(input_size)
         else:
             self.norm = nn.Identity()
 
     def forward(self, x):
-        # x shape is (B, L, H)
-        if self.transpose: x = x.transpose(-1, -2)  # (B, L, H) -> (B, H, L)
+        #if transpose: x = x.transpose(-1, -2)  # (B, H, L) -> (B, L, H)
         x = self.norm(x)
-        if self.transpose: x = x.transpose(-1, -2)  # (B, H, L)-> (B, L, H)
-        return x
-    
+        #if transpose: x = x.transpose(-1, -2)  # (B, L, H) -> (B, H, L)
 
+        return x
+
+
+
+class TransposeBatchNorm(nn.Module):
+    """ A classic batch norm layer, but the input is expected to have shape (B, L, H)
+        and the normalization is performed on the H dimension
+    """
+
+    def __init__(self, input_size):
+        super().__init__()
+        self.input_size = input_size
+        self.norm = nn.BatchNorm1d(input_size)
+
+    def forward(self, x):
+        # x shape is (B, L, H)
+        x = x.transpose(-1, -2)  # (B, L, H) -> (B, H, L)
+        x = self.norm(x)
+        x = x.transpose(-1, -2)  # (B, H, L)-> (B, L, H)
+        return x
+
+class CustomLayerNorm(nn.Module):
+    """ expects shape (B, H, L)
+    """
+
+    def __init__(self, d, scalar=True):
+        super().__init__()
+        self.scalar = scalar
+        if self.scalar:
+            self.m = nn.Parameter(torch.zeros(1))
+            self.s = nn.Parameter(torch.ones(1))
+        else:
+            self.ln = nn.LayerNorm(d)
+
+    def forward(self, x):
+        if self.scalar:
+            s, m = torch.std_mean(x, dim=-2, unbiased=False, keepdim=True)
+            y = (self.s/s) * (x-m+self.m)
+        else:
+            y = self.ln(x.transpose(-1,-2)).transpose(-1,-2)
+        return y
 
 
 

models.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 import opt_einsum as oe
 
 from layers import DSSLayer, S4Layer, TopPooling, InputEncoder, Normalization
-
+from transformer_layers import EncoderLayer, DecoderLayer, PositionalEncoding
 
 
 
@@ -38,12 +38,12 @@ class DSS(nn.Module):
         seed=None,
         **kwargs
     ):
-        assert n_layers > 0, (
-            f"DSS model should have at least one core dss layer, found n_layers = {n_layers}")
-        if seed:
-            torch.manual_seed(seed)
-            if torch.cuda.is_available():
-                torch.cuda.manual_seed_all(seed)
+        #assert n_layers > 0, (
+        #    f"DSS model should have at least one core dss layer, found n_layers = {n_layers}")
+        #if seed:
+        #    torch.manual_seed(seed)
+        #    if torch.cuda.is_available():
+        #        torch.cuda.manual_seed_all(seed)
         super().__init__()
 
         self.input_size = input_size
@@ -59,51 +59,53 @@ class DSS(nn.Module):
             self.layer_norms = {'layer_norm_{}'.format(i): 0. for i in range(n_layers)}
             self.layer_norms.update({'input_layer_norm': 0., 'pooling_layer_norm': 0., 'output_norm': 0.})
 
-        self.input_layer = InputEncoder(data_dim, input_size, mode=encoding, **kwargs)
-        self.normalization_layer = Normalization(input_size, mode=normalization)
-        self.output_layer = nn.Linear(input_size, output_size, bias=bias)
-        self.drop = nn.Dropout(dropout) if dropout > 0.0 else nn.Identity()
+        self.drop = nn.Dropout2d(dropout) if dropout > 0.0 else nn.Identity()
 
-        self.core_blocks = []
+        self.dss_blocks = []
+        self.inner_normalizations = []
 
         for i in range(n_layers):
             # stacker n_layers blocs DSS:
             # DSSLayer (core) + activation + dropout + linear (mixing layer)
-            core_block = nn.Sequential(OrderedDict([
-                ('dss_layer', DSSLayer(input_size=input_size, state_size=state_size, version=self.version, bidirectional=bidirectional, bias=bias, **kwargs)),
-                ('activation', self.activation),
-                #('dropout', nn.Dropout(dropout) if dropout > 0.0 else nn.Identity()),
-                ('dropout', self.drop),
-                ('linear', nn.Linear(input_size, input_size, bias=bias))
-            ]))
-            setattr(self, f'core_block_{i}', core_block)
-            self.core_blocks.append(core_block)
+            dss_layer = DSSLayer(input_size=input_size, state_size=state_size, activation=activation, dropout=dropout, version=self.version, bidirectional=bidirectional, bias=bias, **kwargs)
+            setattr(self, f'core_block_{i}', dss_layer)
+            self.dss_blocks.append(dss_layer)
+            setattr(self, f"inner_normalization_{i}", Normalization(input_size, mode=normalization))
+            self.inner_normalizations.append(getattr(self, f"inner_normalization_{i}"))
 
+        self.normalization_layer = Normalization(input_size, mode=normalization) if prenorm else nn.Identity()
+        self.input_layer = InputEncoder(data_dim, input_size, mode=encoding, **kwargs)
+        self.output_layer = nn.Linear(input_size, output_size, bias=bias)
         # top pooling layer
         self.top_pooling = TopPooling(mode=pooling)
 
-    def forward(self, u):
+    def forward(self, u, transpose=True):
+        """ Input u should be of shape (B, L) if encoding is 'embedding', else (B, L, data_dim)"""
         x = self.input_layer(u)
         if self.track_norms: self.layer_norms['input_layer_norm'] += self.compute_layer_norm(x)
-        for i, block in enumerate(self.core_blocks):
+        if transpose: x = x.transpose(-1, -2)  # (B H L)
+        for i, layer in enumerate(self.dss_blocks):
             if self.residual: y = x
-            if self.prenorm: x = self.normalization_layer(x)
+            if self.prenorm: x = self.inner_normalizations[i](x)
             # DSS core computation + activation + dropout + linear mixing
-            x = block(x)
-            if self.residual: x = self.drop(x) + y
-            if not self.prenorm: x = self.normalization_layer(x)
-            if self.track_norms: self.layer_norms['layer_norm_{}'.format(i)] += self.compute_layer_norm(x)
-        if self.prenorm: x = self.normalization_layer(x)
+            x = layer(x)  # (B L H) / (B H L)
+            if self.residual: x = self.drop(x)  + y
+            if not self.prenorm: x = self.inner_normalizations[i](x)
+            if self.track_norms: self.layer_norms['layer_norm_{}'.format(i)] += self.compute_layer_norm(x, transpose=transpose)
+        # if prenorm, just the identity
+        x = self.normalization_layer(x)
+        # pooling along the length dimension
+        if transpose: x = x.transpose(-1, -2)  # (B H L) -> (B L H)
         x = self.top_pooling(x)
-        if self.track_norms: self.layer_norms['pooling_layer_norm'] += self.compute_layer_norm(x, is_sequence=False)
+        if self.track_norms: self.layer_norms['pooling_layer_norm'] += self.compute_layer_norm(x, is_sequence=False, transpose=transpose)
         x = self.output_layer(x)
-        if self.track_norms: self.layer_norms['output_norm'] += self.compute_layer_norm(x, is_sequence=False)
+        if self.track_norms: self.layer_norms['output_norm'] += self.compute_layer_norm(x, is_sequence=False, transpose=transpose)
         return x
 
     def __str__(self):
         ret_str = str(self.input_layer) + "\n"
         ret_str += str(self.core_block_0) + "\n"
-        ret_str += "X {}".format(len(self.core_blocks)) + "\n"
+        ret_str += "X {}".format(len(self.dss_blocks)) + "\n"
         ret_str += str(self.normalization_layer) + "\n"
         ret_str += str(self.top_pooling) + "\n"
         ret_str += str(self.output_layer)
@@ -116,13 +118,13 @@ class DSS(nn.Module):
         
         norms = {}
         with torch.no_grad():
-            for i, block in enumerate(self.core_blocks):
+            for i, block in enumerate(self.dss_blocks):
                 k = block.dss_layer.kernel(L)
                 norms['norms/kernel_{}'.format(i)] = k[0].norm().item() / k[0].numel()
                 norms['norms/D_{}'.format(i)] = block.dss_layer.D.norm().item() / block.dss_layer.D.numel()
         return norms
 
-    def compute_layer_norm(self, x, is_sequence=True):
+    def compute_layer_norm(self, x, is_sequence=True, transpose=True):
         """ Compute the norms of the first item of the sequence, averaged over batches
         """
 
@@ -131,10 +133,11 @@ class DSS(nn.Module):
             y = x.mean(dim=0)
             # keep only the first item of the sequence
             if is_sequence:
-                y = y[0]
+                if transpose: y = y[..., 0]  # (H) --- if x is (B H L)
+                else: y = y[0]
 
         return y.norm().item() / y.numel()
-    
+
     def average_layer_norms(self, n_batches):
         for k in self.layer_norms.keys():
             self.layer_norms[k] /= n_batches
@@ -231,10 +234,7 @@ class S4(DSS):
 
 
 
-
-
-
-class Transformer(nn.Module):
+class TransformerEncoder(nn.Module):
 
     def __init__(
         self,
@@ -244,6 +244,7 @@ class Transformer(nn.Module):
         seq_length,
         bias=True,
         encoding=None,
+        dropout=0.1,
         pooling='last',     # top pooling mode - 'last' or 'average' or 'manytomany'
         seed=None,
         **kwargs
@@ -257,6 +258,10 @@ class Transformer(nn.Module):
         self.seq_length = seq_length
         self.input_size = input_size
         self.output_size = output_size
+        if kwargs.get('padding_idx') is not None:
+            self.padding_idx = kwargs.get('padding_idx')
+        else:
+            self.padding_idx = None
 
         #self.embedding = nn.Embedding(data_dim, input_size)
         self.input_layer = InputEncoder(data_dim, input_size, mode=encoding, **kwargs)
@@ -264,20 +269,83 @@ class Transformer(nn.Module):
         encoder_layer = nn.TransformerEncoderLayer(
             d_model=input_size,
             nhead=8,
-            batch_first=True
+            batch_first=True,
+            norm_first=True
         )
         self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=6)
         self.output_layer = nn.Linear(input_size, output_size, bias=bias)
+        self.dropout = nn.Dropout(dropout) if dropout > 0.0 else nn.Identity()
         
         # top pooling layer
         self.top_pooling = TopPooling(mode=pooling)
 
+    def generate_causal_mask(self, seq_length, device):
+        return torch.triu(
+            torch.ones(seq_length, seq_length, device=device),
+            diagonal=1
+        ).bool()
+
     def forward(self, u):
+        #mask = self.generate_mask(u)
+        mask = self.generate_causal_mask(self.seq_length, u.device)
         batch_size = u.shape[0]
         positions = torch.arange(0, self.seq_length, device=u.device).unsqueeze(0).expand(batch_size, self.seq_length)
+        src_key_padding_mask = None if self.padding_idx is None else (u == self.padding_idx)
 
-        x = self.input_layer(u) + self.pos_embedding(positions)
-        x = self.transformer(x)  # Pass the embeddings through the Transformer
+        x = self.dropout(
+            self.input_layer(u) + self.pos_embedding(positions)
+        )
+        x = self.transformer(x, mask=mask, src_key_padding_mask=src_key_padding_mask)  # Pass the embeddings through the Transformer
         x = self.top_pooling(x)
         x = self.output_layer(x)
         return x
+
+
+
+
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        tgt_vocab_size,
+        d_model,
+        num_heads,
+        num_layers,
+        d_ff,
+        max_seq_length,
+        dropout
+    ):
+        super(Transformer, self).__init__()
+        self.encoder_embedding = nn.Embedding(src_vocab_size, d_model)
+        self.decoder_embedding = nn.Embedding(tgt_vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, max_seq_length)
+
+        self.encoder_layers = nn.ModuleList([EncoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+        self.decoder_layers = nn.ModuleList([DecoderLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
+
+        self.fc = nn.Linear(d_model, tgt_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def generate_mask(self, src, tgt):
+        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
+        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
+        seq_length = tgt.size(1)
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
+        tgt_mask = tgt_mask & nopeak_mask
+        return src_mask, tgt_mask
+
+    def forward(self, src, tgt):
+        src_mask, tgt_mask = self.generate_mask(src, tgt)
+        src_embedded = self.dropout(self.positional_encoding(self.encoder_embedding(src)))
+        tgt_embedded = self.dropout(self.positional_encoding(self.decoder_embedding(tgt)))
+
+        enc_output = src_embedded
+        for l in self.encoder_layers:
+            enc_output = l(enc_output, src_mask)
+
+        dec_output = tgt_embedded
+        for l in self.decoder_layers:
+            dec_output = l(dec_output, enc_output, src_mask, tgt_mask)
+
+        output = self.fc(dec_output)
+        return output